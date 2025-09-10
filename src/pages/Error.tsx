dc4a24448ab1fe2cb56551105893c665c112f85f
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-09-10 20:27:47 +0200

    style/start setting up styling and implementing AF design system

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,3 +1,26 @@
+import { ErrorPageStatusCodes } from '@digi/arbetsformedlingen';
+import {
+  DigiNotificationErrorPage,
+  DigiLinkInternal,
+} from '@digi/arbetsformedlingen-react';
+
 export const Error = () => {
-  return <div>Error</div>;
+  return (
+    <DigiNotificationErrorPage
+      afHttpStatusCode={ErrorPageStatusCodes.FORBIDDEN}
+    >
+      <ul slot="links">
+        <li>
+          <DigiLinkInternal afHref="/" afVariation="small">
+            Till föregående sida
+          </DigiLinkInternal>
+        </li>
+        <li>
+          <DigiLinkInternal afHref="/" afVariation="small">
+            Till startsidan
+          </DigiLinkInternal>
+        </li>
+      </ul>
+    </DigiNotificationErrorPage>
+  );
 };
