28bde77ab35cc9216f407edafa534cc471d98e41
Oskar <Oskarlundberg@hotmail.com>
2025-09-10 11:26:47 +0200

    feat(service): Implements api functions service.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import type { Ad } from "../models/Ad";
+import { getAds, getLatestAds } from "../services/adService";
+import { Link } from "react-router";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [ads, setAds] = useState<Ad[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const ads = await getLatestAds();
+      setAds(ads);
+    };
+
+    if (ads.length > 0) return;
+
+    getData();
+  });
+  return (
+    <div>
+      <h2>Home</h2>
+      {ads.map((a) => (
+        <div key={a.id}>
+          <Link to={`/ads/${a.id}`}>
+            <h3>{a.headline}</h3>
+            <p>{a.employer.name}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
---
+++ src/services/adService.ts
@@ -0,0 +1,32 @@
+import type { Ad } from "../models/Ad";
+import type { JobsearchResponse } from "../models/JobsearchResponse";
+import { get } from "./serviceBase";
+
+const BASE_URL = "https://jobsearch.api.jobtechdev.se/";
+const SEARCH = "search?occupation-group=DJh5_yyF_hEM";
+// q=Stockholm%20frontend&offset=0&limit=10
+// "q=Stockholm%20frontend" Is the search query. %20 = blankspace.
+// occupation-group=DJh5_yyF_hEM targets jobs within the software development career.
+// offset means where to start in the list and limit is how many to fetch.
+
+export const getAds = async (searchValue: string) => {
+  const data = await get<JobsearchResponse>(
+    `${BASE_URL}${SEARCH}&q=${searchValue}&offset=0&limit=10`
+  ); // offset part will be dynamic so will have to be replaced
+
+  return data.hits;
+};
+
+export const getAdById = async (id: string) => {
+  const data = await get<Ad>(`${BASE_URL}ad/${id}`);
+
+  return data;
+};
+
+export const getLatestAds = async () => {
+  const data = await get<JobsearchResponse>(
+    `${BASE_URL}${SEARCH}&offset=0&limit=10&sort=pubdate-desc`
+  );
+
+  return data.hits;
+};
