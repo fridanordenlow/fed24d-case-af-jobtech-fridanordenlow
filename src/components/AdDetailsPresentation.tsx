f2c1545fd393e2a07330e51c0dcdf49cc0ff2194
M-Lenvik <lenvik.marie@gmail.com>
2025-09-11 22:22:15 +0200

    fix: AdDetailsPresentation styling (inline for now)

--- src/components/AdDetailsPresentation.tsx
+++ src/components/AdDetailsPresentation.tsx
@@ -1,14 +1,11 @@
 import {
   DigiLayoutBlock,
-  DigiLayoutColumns,
   DigiLayoutContainer,
   DigiTypography,
 } from '@digi/arbetsformedlingen-react';
 import type { AdExt } from '../models/AdExt';
 import {
   LayoutBlockVariation,
-  LayoutColumnsElement,
-  LayoutColumnsVariation,
   TypographyVariation,
 } from '@digi/arbetsformedlingen';
 
@@ -19,57 +16,62 @@ interface Props {
 export const AdDetailsPresentation = ({ ad }: Props) => {
   return (
     <>
-      <DigiLayoutBlock
-        afVariation={LayoutBlockVariation.PRIMARY}
-        style={{
-          border: '2px solid red',
-        }}
-      >
-        <h1>AdDetailsPresentation</h1>
+      <DigiLayoutBlock afVariation={LayoutBlockVariation.PRIMARY}>
+        <div
+          style={{
+            padding: '1em',
+            backgroundColor: 'lightgreen',
+            border: '2px solid red',
+          }}
+        >
+          <DigiTypography afVariation={TypographyVariation.LARGE}>
+            <h1>{ad.headline}</h1>
+            <h2>{ad.employer.name}</h2>
+          </DigiTypography>
+          <DigiLayoutContainer af-margin-all>
+            <div style={{ display: 'flex', width: '100%',  gap: '5rem', marginTop: '2em',}}>
+              <div style={{ width: '25%'}}>
+                <DigiTypography afVariation={TypographyVariation.SMALL}>
+                  <section>
+                    <p>
+                      <strong>Plats:</strong> {ad.workplace_address.municipality}{' i '}
+                      {ad.workplace_address.region}
+                    </p>
+                    <p>
+                      <strong>Publicerad:</strong>{' '}
+                      {new Date(ad.publication_date).toLocaleDateString()}
+                    </p>
+                    <p>
+                      <strong>Deadline:</strong>{' '}
+                      {new Date(ad.application_deadline).toLocaleDateString()}
+                    </p>
+                    <p>
+                      <strong>
+                        Här skulle kvalifikationer vara bra, men jag hittar inte
+                        hur jag ska göra:
+                      </strong>
+                    </p>
+                  </section>
+                </DigiTypography>
+              </div>
 
-        <DigiLayoutContainer af-margin-all>
-          <DigiLayoutColumns afVariation={LayoutColumnsVariation.TWO}>
-            <div style={{ flex: '1 0 25%', border: '2px solid blue' }}>
-              <DigiTypography afVariation={TypographyVariation.SMALL}>
-                <section>
-                  <h1>{ad.headline}</h1>
-                  <h2>{ad.employer.name}</h2>
-                  <p>
-                    <strong>Plats:</strong> {ad.workplace_address.region}{' '}
-                    {ad.workplace_address.municipality}
-                  </p>
-                  <p>
-                    <strong>Publicerad:</strong>{' '}
-                    {new Date(ad.publication_date).toLocaleDateString()}
-                  </p>
-                  <p>
-                    <strong>Deadline:</strong>{' '}
-                    {new Date(ad.application_deadline).toLocaleDateString()}
-                  </p>
-                  <p>
-                    <strong>
-                      Här skulle kvalifikationer vara bra, men jag hittar inte
-                      hur jag ska göra:
-                    </strong>
-                  </p>
-                </section>
-              </DigiTypography>
-            </div>
+              <div style={{ width: '1px', backgroundColor: 'gray' }}></div>
 
-            <div style={{ flex: '1 0 75%', border: '2px solid red' }}>
-              <DigiTypography afVariation={TypographyVariation.SMALL}>
-                <section>
-                  <h3>Tjänstbeskrivning</h3>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: ad.description.text_formatted,
-                    }}
-                  ></p>
-                </section>
-              </DigiTypography>
+              <div style={{ width: '75%'}}>
+                <DigiTypography afVariation={TypographyVariation.SMALL}>
+                  <section>
+                    <h3>Tjänstbeskrivning</h3>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: ad.description.text_formatted,
+                      }}
+                    ></p>
+                  </section>
+                </DigiTypography>
+              </div>
             </div>
-          </DigiLayoutColumns>
-        </DigiLayoutContainer>
+          </DigiLayoutContainer>
+        </div>
       </DigiLayoutBlock>
     </>
   );
