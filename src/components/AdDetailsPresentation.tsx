55151db990c80fb5d809074d9f9d1f2126fb2f3e
M-Lenvik <lenvik.marie@gmail.com>
2025-09-11 23:41:16 +0200

    Update: AdExt model with extends Ad and additional fields from API Update: AdDetailsPresentation fields only if data exists

--- src/components/AdDetailsPresentation.tsx
+++ src/components/AdDetailsPresentation.tsx
@@ -29,35 +29,136 @@ export const AdDetailsPresentation = ({ ad }: Props) => {
             <h2>{ad.employer.name}</h2>
           </DigiTypography>
           <DigiLayoutContainer af-margin-all>
-            <div style={{ display: 'flex', width: '100%',  gap: '5rem', marginTop: '2em',}}>
-              <div style={{ width: '25%'}}>
+            <div
+              style={{
+                display: 'flex',
+                width: '100%',
+                gap: '5rem',
+                marginTop: '2em',
+              }}
+            >
+              <div style={{ width: '25%' }}>
                 <DigiTypography afVariation={TypographyVariation.SMALL}>
                   <section>
-                    <p>
-                      <strong>Plats:</strong> {ad.workplace_address.municipality}{' i '}
-                      {ad.workplace_address.region}
-                    </p>
-                    <p>
-                      <strong>Publicerad:</strong>{' '}
-                      {new Date(ad.publication_date).toLocaleDateString()}
-                    </p>
-                    <p>
-                      <strong>Deadline:</strong>{' '}
-                      {new Date(ad.application_deadline).toLocaleDateString()}
-                    </p>
-                    <p>
-                      <strong>
-                        Här skulle kvalifikationer vara bra, men jag hittar inte
-                        hur jag ska göra:
-                      </strong>
-                    </p>
+                    <div
+                      style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '0.5em',
+                      }}
+                    >
+                      {ad.workplace_address?.municipality &&
+                        ad.workplace_address?.region && (
+                          <p>
+                            <strong>Plats:</strong>{' '}
+                            {ad.workplace_address.municipality} i{' '}
+                            {ad.workplace_address.region}
+                          </p>
+                        )}
+
+                      {ad.occupation_field?.label && (
+                        <p>
+                          <strong>Yrkesområde:</strong>{' '}
+                          {ad.occupation_field.label}
+                        </p>
+                      )}
+                      {ad.occupation_group?.label && (
+                        <p>
+                          <strong>Yrkesgrupp:</strong>{' '}
+                          {ad.occupation_group.label}
+                        </p>
+                      )}
+
+                      {ad.employment_type?.label && (
+                        <p>
+                          <strong>Anställningstyp:</strong>{' '}
+                          {ad.employment_type.label}
+                        </p>
+                      )}
+
+                      {ad.working_hours_type?.label && (
+                        <p>
+                          <strong>Arbetstid:</strong>{' '}
+                          {ad.working_hours_type.label}
+                        </p>
+                      )}
+
+                      {ad.experience_required && (
+                        <p>
+                          <strong>Yrkeserfarenhet krävs:</strong> Ja
+                        </p>
+                      )}
+                      {typeof ad.driving_license_required === 'boolean' && (
+                        <p>
+                          <strong>Körkort krävs:</strong>{' '}
+                          {ad.driving_license_required ? 'Ja' : 'Nej'}
+                        </p>
+                      )}
+
+                      {ad.application_contacts?.length ? (
+                        <div>
+                          <h3>Kontaktperson</h3>
+                          {ad.application_contacts.map((contact, index) => (
+                            <div key={index}>
+                              {contact.name && (
+                                <p>
+                                  <strong>{contact.name}</strong>
+                                </p>
+                              )}
+                              {contact.description && (
+                                <p>{contact.description}</p>
+                              )}
+                              {contact.email && (
+                                <p>
+                                  <strong>Email:</strong> {contact.email}
+                                </p>
+                              )}
+                              {contact.telephone && (
+                                <p>
+                                  <strong>Telefon:</strong> {contact.telephone}
+                                </p>
+                              )}
+                            </div>
+                          ))}
+                        </div>
+                      ) : null}
+
+                      {ad.publication_date && (
+                        <p>
+                          <strong>Publicerad:</strong>{' '}
+                          {new Date(ad.publication_date).toLocaleDateString()}
+                        </p>
+                      )}
+
+                      {ad.application_deadline && (
+                        <p>
+                          <strong>Deadline:</strong>{' '}
+                          {new Date(
+                            ad.application_deadline
+                          ).toLocaleDateString()}
+                        </p>
+                      )}
+
+                      {ad.application_details?.url && (
+                        <p>
+                          <strong>Ansökan:</strong>{' '}
+                          <a
+                            href={ad.application_details.url}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {ad.application_details.url}
+                          </a>
+                        </p>
+                      )}
+                    </div>
                   </section>
                 </DigiTypography>
               </div>
 
               <div style={{ width: '1px', backgroundColor: 'gray' }}></div>
 
-              <div style={{ width: '75%'}}>
+              <div style={{ width: '75%' }}>
                 <DigiTypography afVariation={TypographyVariation.SMALL}>
                   <section>
                     <h3>Tjänstbeskrivning</h3>
