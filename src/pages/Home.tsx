907fb0ad08815a06d416ccacab20da505723366b
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-30 15:39:44 +0100

    move job chart to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Welcome } from '../components/Welcome';
 import { LatestAdsPresentation } from '../components/LatestAdsPresentation';
+import { JobsStatisticsChart } from '../components/JobsStatisticsChart';
 
 export const Home = () => {
   return (
     <>
       <Welcome />
+      <JobsStatisticsChart />
       <LatestAdsPresentation />
     </>
   );
