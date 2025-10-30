ff4cf68dec36eb147d512dc99bddb7a703ed6389
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-10-30 15:37:54 +0100

    update job chart to use ads from context

---
+++ src/components/JobsStatisticsChart.tsx
@@ -0,0 +1,42 @@
+// JobsPerMunicipalityChart.tsx
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+import { countJobsPerMunicipality } from '../helpers/countJobsPerMunicipality';
+import { useAdContext } from '../hooks/useAdContext';
+import { DigiTypography } from '@digi/arbetsformedlingen-react';
+import { ChartWrapper } from './styled/Wrappers';
+
+export const JobsStatisticsChart = () => {
+  const { ads } = useAdContext();
+
+  if (ads.length === 0) {
+    return <p>Ingen statistik tillgänglig just nu.</p>;
+  }
+
+  const data = countJobsPerMunicipality(ads);
+
+  return (
+    <ChartWrapper>
+      <DigiTypography>
+        <h3>Översikt över antal jobb per ort</h3>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip
+              cursor={false}
+              formatter={(value: number) => [`${value}`, 'Antal jobb']}
+            />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </DigiTypography>
+    </ChartWrapper>
+  );
+};
