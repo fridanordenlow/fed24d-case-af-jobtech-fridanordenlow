cb982aeb28da500108a151f229ba7a163c5bb4a4
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-11-10 16:21:23 +0100

    move initial fetching of ads to from Ads to adProvider

--- src/contexts/AdProvider.tsx
+++ src/contexts/AdProvider.tsx
@@ -1,6 +1,7 @@
-import { useState, type ReactNode } from 'react';
+import { useEffect, useState, type ReactNode } from 'react';
 import { AdContext } from './AdContext';
 import type { AdExt } from '../models/AdExt';
+import { getAds } from '../services/adService';
 
 type AdProviderProps = { children: ReactNode };
 
@@ -12,6 +13,28 @@ export const AdProvider = ({ children }: AdProviderProps) => {
   const [totalPages, setTotalPages] = useState(1);
   const [totalResult, setTotalResult] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [hasFetched, setHasFetched] = useState(false);
+
+  useEffect(() => {
+    const setInitialAds = async () => {
+      try {
+        setLoading(true);
+        setHasFetched(true);
+        setCurrentPage(1);
+        setError(null);
+        const data = await getAds('', 0, 100);
+        setAds(data.ads);
+        setTotalResult(data.totalHits.value > 100 ? 100 : data.totalHits.value);
+      } catch (err) {
+        console.error('Error fetching ads:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    // if (hasFetched) return;
+    if (!hasFetched) setInitialAds();
+  }, [hasFetched]);
 
   return (
     <AdContext.Provider
--- src/pages/Ads.tsx
+++ src/pages/Ads.tsx
@@ -3,38 +3,39 @@ import {
   DigiLoaderSpinner,
   DigiTypography,
 } from '@digi/arbetsformedlingen-react';
-import { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router';
+// import { useEffect, useState } from 'react';
+// import { useLoaderData } from 'react-router';
 import { LoaderSpinnerSize } from '@digi/arbetsformedlingen';
 import { SearchForm } from '../components/SearchForm';
 import { AdsPresentation } from '../components/AdsPresentation';
 import { useAdContext } from '../hooks/useAdContext';
-import type { AdsLoader } from '../loaders/adsLoader';
+// import type { AdsLoader } from '../loaders/adsLoader';
 import { AdsPagination } from '../components/AdsPagination';
 import { LoaderSpinnerWrapper } from '../components/styled/Wrappers';
 
 export const Ads = () => {
-  const loaderData = useLoaderData<AdsLoader>();
-  const { ads, setAds, setTotalResult, setCurrentPage, loading, error } =
-    useAdContext();
-  const [hasFetched, setHasFetched] = useState(false);
+  const { ads, loading, error } = useAdContext();
+  // const loaderData = useLoaderData<AdsLoader>();
+  // const { ads, setAds, setTotalResult, setCurrentPage, loading, error } =
+  //   useAdContext();
+  // const [hasFetched, setHasFetched] = useState(false);
 
-  useEffect(() => {
-    const setInitialAds = () => {
-      setAds(loaderData.ads);
-      setHasFetched(true);
-      setCurrentPage(1);
-      if (loaderData.total.value > 100) {
-        setTotalResult(100);
-      } else {
-        setTotalResult(loaderData.total.value);
-      }
-    };
+  // useEffect(() => {
+  //   const setInitialAds = () => {
+  //     setAds(loaderData.ads);
+  //     setHasFetched(true);
+  //     setCurrentPage(1);
+  //     if (loaderData.total.value > 100) {
+  //       setTotalResult(100);
+  //     } else {
+  //       setTotalResult(loaderData.total.value);
+  //     }
+  //   };
 
-    if (hasFetched) return;
+  //   if (hasFetched) return;
 
-    setInitialAds();
-  });
+  //   setInitialAds();
+  // });
 
   return (
     <DigiLayoutContainer afNoGutter>
