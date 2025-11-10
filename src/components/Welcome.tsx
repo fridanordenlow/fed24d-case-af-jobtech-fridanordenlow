77fed6d51ac04def277a9854d5c133a3ea759d35
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-11-10 16:57:24 +0100

    update text content

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -11,16 +11,19 @@ export const Welcome = () => {
         <DigiLayoutContainer>
           <h3>Välkommen till Tech Reqruitments!</h3>
           <p>
-            På Tech Reqruitments kan du enkelt söka bland lediga jobb inom IT och mjukvaruutveckling. Vi listar alla lediga jobb på platsbanken, utan att du får irrelevanta sökträffar.
+            På Tech Reqruitments kan du enkelt söka bland lediga jobb inom IT
+            och mjukvaruutveckling. Vi listar alla lediga jobb på platsbanken,
+            utan att du får irrelevanta sökträffar.
           </p>
-            
+
           <p>
-            Nedan ser du de tio senast inkomna jobben så du enkelt kan vara första kandidat att söka nyutlagda jobb. På söksidan kan du fritextsöka för att hitta annonser som matchar dina intressen och din ort. Där får du även en översikt över var flest jobb inom yrkesområdet du är intresserad av finns.
+            Nedan ser du de tio senast inkomna jobben så du enkelt kan vara
+            första kandidat att söka nyutlagda jobb. På söksidan kan du
+            fritextsöka för att hitta annonser som matchar dina intressen och
+            din ort.
           </p>
-            
-          <p><strong>
-            Gå in på sök jobb och upptäck de jobb som passar dig bäst!
-          </strong></p>
+
+          <p>Gå in på sök jobb och upptäck de jobb som passar dig bäst!</p>
         </DigiLayoutContainer>
       </DigiTypography>
     </WelcomeWrapper>
